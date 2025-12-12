f37449f26632f8506874f6d9bf3b5e72486e7392
jin-zero0 <jin-zero0@github.com>
2025-12-12 15:18:20 +0900

    메인과 데브 브런치 분리

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,23 +3,17 @@ import HeroSection from './components/HeroSection';
 import AboutSection from './components/AboutSection';
 import CustomerSection from './components/CustomerSection';
 import ServicesSection2 from './components/ServicesSection2';
-import AiVisionSection from './components/AiVisionSection';
-import AiResourcesSection from './components/AiResourcesSection';
-import SolutionsSection from './components/SolutionsSection';
 import StatsSection from './components/StatsSection';
-import CareersSection from './components/CareersSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import TeamSection from './components/TeamSection';
 import PartnerSection from './components/PartnerSection';
 import ESGSection from './components/ESGSection';
 import NewsSection from './components/NewsSection';
-import HistorySection from './components/HistorySection';
 import ECOPostSection from './components/ECOPostSection';
 import LocationSection from './components/LocationSection';
 import FloatingActions from './components/FloatingActions';
 import CareersContent from './components/CareersContent';
-import CertificationSection from './components/CertificationSection';
 import EvolutionSection from './components/EvolutionSection';
 
 export default function Home() {
@@ -29,23 +23,15 @@ export default function Home() {
       <HeroSection />
       <ServicesSection2 />
       <AboutSection />
-
       <CustomerSection />
-
-      {/* <SolutionsSection /> */}
       <StatsSection />
-
-      {/* <ContactSection /> */}
       <TeamSection />
       <PartnerSection />
-      {/* <CertificationSection /> */}
       <ECOPostSection />
       <ESGSection />
       <NewsSection />
-      {/* <HistorySection /> */}
       <EvolutionSection />
       <CareersContent />
-
       <LocationSection />
       <ContactSection />
       <Footer />
